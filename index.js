730fbef9a859a52624d08936153bdb1cdb2c8d20
tprayogaa <tprayogaa26@gmail.com>
2022-05-04 20:04:44 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+require('dotenv').config()
+
+const router = require('./src/routes')
+
+const app = express()
+const port = 5000
+
+app.use(express.json())
+
+app.use('/api/v1/', router)
+app.use('/uploads', express.static('uploads'))
+
+
+app.listen(port, (() => console.log(`Listening on port ${port}`)))
